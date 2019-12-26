151125594702e9afde9d47d8564b26ac05376ce3
phatdroid94 <phatdroid94@gmail.com>
2019-12-26 20:17:00 +0700

    fix update

--- update.php
+++ update.php
@@ -6,17 +6,27 @@
     $name = $_POST["name"];
     $yearofbirth = $_POST["yearofbirth"];
     $address = $_POST["address"];
+    if($id != ''){
 
-    if($id != '' && $name != '' && $yearofbirth != '' && $address != ''){
-        $query = "UPDATE sinhvien SET name = '$name' , yearofbirth = '$yearofbirth' ,address='$address' WHERE id = $id";
-        $result = $con->query($query);
-    
-        if($result){
-            echo json_encode(new Response("true","Cập nhật sinh viên thành công"));
+        $queryCheckId = "SELECT id FROM sinhvien WHERE id = $id";
+
+        $instance = $con->query($queryCheckId);
+        if($instance->num_rows > 0 ){
+            if($id != '' && $name != '' && $yearofbirth != '' && $address != ''){
+                $query = "UPDATE sinhvien SET name = '$name' , yearofbirth = '$yearofbirth' ,address='$address' WHERE id = $id";
+                $result = $con->query($query);
+            
+            if($result){
+                echo json_encode(new Response("true","Cập nhật sinh viên thành công"));
+            }else{
+                echo json_encode(new Response("false","Cập nhật sinh viên thất bại"));
+            }
         }else{
-            echo json_encode(new Response("false","Cập nhật sinh viên thất bại"));
-        }
+            echo json_encode(new Response("false","Dữ liệu không đầy đủ"));
+        }  
     }else{
-        echo json_encode(new Response("false","Dữ liệu không đầy đủ"));
-    }    
+        echo json_encode(new Response("false","Sinh viên không tồn tại"));
+    }
+}
+      
 ?>
